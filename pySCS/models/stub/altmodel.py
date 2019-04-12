afe29cce75bf64c2104157501d73b0e73c0bed08
Dave van Stein <dvanstein@xebia.com>
2019-04-12 21:47:10 +0200

    cli params and stub models

---
+++ pySCS/models/stub/altmodel.py
@@ -0,0 +1,10 @@
+# Control frameworks to use
+Threats = {}
+
+# add threatlists
+addThreatList('test_sample.csv')
+addThreatList('test_sample2.csv')
+# addThreatList('default.csv')
+# addThreatList('gdpr.csv')
+
+
--- pySCS/pySCS.py
+++ pySCS/pySCS.py
@@ -14,6 +14,9 @@
 import os
 from  os import path
 import sys
+from sys import stderr, exit
+import argparse
+
 
 def str_to_class(classname):
     sumelements = []
@@ -44,14 +47,68 @@ def addThreatList(csv_file):
     # Update global threatlist
     Threats.update(temp_dict)
 
-#from pytm.threats import Threats
-# Initialize threatlist
 
-# addThreatList('gdpr.csv')
+# Program start
 
-# STUB - test to load model.py
-exec(open("models/stub/model.py").read())
+# First parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('folder', help='required; folder containing the model.py to process')
+parser.add_argument('--file', help='alternative filename (default = model.py')
+parser.add_argument('--dfd', action='store_true', help='output DFD')
+parser.add_argument('--seq', action='store_true', help='output sequential diagram')
+parser.add_argument('--report', help='output report using the specified template file')
+parser.add_argument('--list', action='store_true', help='list controls used in model')
+parser.add_argument('--listfull', action='store_true', help='same as --list but with full descriptions')
+parser.add_argument('--exclude', help='specify threat IDs to be ignored')
+parser.add_argument('--describe', help='describe the contents of a given class')
+parser.add_argument('--debug', action='store_true', help='print debug messages')
 
-# STUB - show threat dictionary
-for key, value in Threats.items() :
-	print("{i}:  {d} - from  {s} to {t} when {c}. Mitigation: {m}".format(i=key, d=Threats[key]["description"], s=Threats[key]["source"], t=Threats[key]["target"], c=Threats[key]["condition"], m=Threats[key]["mitigation"]))
+args = parser.parse_args()
+
+# check provided folder location
+model_location = args.folder
+if os.path.exists(model_location) == False:
+	stderr.write("Folder not found.")
+	exit(-1)	
+
+# check if alternative filename is provided
+if args.file is not None:
+	model_name = args.file
+else:
+	model_name = "model.py"
+
+# parse model
+model_file = os.path.join(model_location, model_name)
+print("Processing: {l}".format(l=model_file))
+try:
+	exec(open(model_file).read())
+except Exception:
+	stderr.write("Model {m} not found.".format(m=model_name))
+	exit(-1)
+
+# list used controls in model (either in short or full description)
+if args.list is True:
+	for key, value in Threats.items() :
+		print("{i} - {d}".format(i=key, d=Threats[key]["description"]))
+if args.listfull is True:
+	for key, value in Threats.items() :
+		print("{i} - {d} \n  from\t{s} \n  to\t{t} \n  when\t{c}\n  Mitigation: {m}".format(i=key, d=Threats[key]["description"], s=Threats[key]["source"], t=Threats[key]["target"], c=Threats[key]["condition"], m=Threats[key]["mitigation"]))
+
+# FIXME BEGIN
+# if args.dfd is True and _args.seq is True:
+#     stderr.write("Cannot produce DFD and sequential diagrams in the same run.\n")
+#     exit(0)
+# if args.report is not None:
+#     TM._template = args.report
+# if args.exclude is not None:
+#     TM._threatsExcluded = args.exclude.split(",")
+# if args.describe is not None:
+#     try:
+#         one_word = args.describe.split()[0]
+#         c = eval(one_word)
+#     except Exception:
+#         stderr.write("No such class to describe: {}\n".format(args.describe))
+#         exit(-1)
+#     print(args.describe)
+#     [print("\t{}".format(i)) for i in dir(c) if not callable(i) and match("__", i) is None]
+# FIXME END
